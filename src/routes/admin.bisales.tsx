74c5456e366b0648f24e849d75256ecf6c4b37b2
Twylahmike <michaelsmiking07@gmail.com>
2026-05-17 15:25:30 +0300

    Update admin.bisales.tsx

--- src/routes/admin.bisales.tsx
+++ src/routes/admin.bisales.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/admin/bi/sales")({
+export const Route = createFileRoute("/admin/bisales")({
   component: SalesBI,
 });
 
